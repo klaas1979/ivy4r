77276587b92f5a96e8dcd8b276d954b5cd92a328
Klaas Prause <klaas.prause@googlemail.com>
2009-10-19 10:21:25 +0200

    Use gemcutter as repository

--- lib/ivy4r.rb
+++ lib/ivy4r.rb
@@ -35,7 +35,7 @@ is
   }
 =end
 class Ivy4r
-  VERSION = '0.7.3'
+  VERSION = '0.8.0'
 
   # Set the ant home directory to load ant classes from if no custom __antwrap__ is provided
   # and the default provided ant version 1.7.1 should not be used.
