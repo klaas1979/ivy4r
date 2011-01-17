386c772509465293c0e722aa2ad245da5c339a47
kp <kp@b231d3c1-3727-4d2b-b224-6530787e4ff7>
2011-01-17 12:12:37 +0000

    Added install task
    git-svn-id: svn://svn/ivy4r/trunk@86130 b231d3c1-3727-4d2b-b224-6530787e4ff7

---
+++ lib/ivy/install.rb
@@ -0,0 +1,32 @@
+require 'ivy/target'
+
+module Ivy
+  class Install < Ivy::Target
+    def parameter
+      [
+        Parameter.new(:from, true),
+        Parameter.new(:to, true),
+        Parameter.new(:organisation, true),
+        Parameter.new(:module, true),
+        Parameter.new(:branch, false),
+        Parameter.new(:revision, true),
+        Parameter.new(:type, false),
+        Parameter.new(:validate, false),
+        Parameter.new(:overwrite, false),
+        Parameter.new(:transitive, false),
+        Parameter.new(:matcher, false),
+        Parameter.new(:settingsRef, false),
+        Parameter.new(:haltonfailure, false)
+      ]
+    end
+
+    protected
+    def execute_ivy
+      call_nested :ivy_install => params
+    end
+
+    def result_property_values
+      nil
+    end
+  end
+end
